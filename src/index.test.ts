2d141ec7e4eace1760fc54cb4d3e98608427cc52
Meithal <hurin8888@gmail.com>
2023-04-17 22:46:32 +0200

    typos

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,10 +3,12 @@ import {describe, test, expect, beforeAll, afterAll} from "@jest/globals";
 import {Server} from "http";
 
 let server: Server;
+
 beforeAll(() => {
     setTimeout(() => server = createServer(app, 3000), 0);
-})
+});
 afterAll(async () => server.close(() => 0));
+
 describe("index.ts hello", () => {
 
     test("Main route to return hello", async () => {
@@ -17,5 +19,5 @@ describe("index.ts hello", () => {
         const res = await f.text();
 
         expect(res).toBe('Hello world');
-    })
-})
+    });
+});
