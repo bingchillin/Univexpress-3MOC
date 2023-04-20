7bf329e7a6f99cbb56bb484f22e56e8e5c53e1e4
Meithal <hurin8888@gmail.com>
2023-04-20 21:55:13 +0200

    fix gitignore

--- src/ihm/express/maquettes.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.maquettesRouter = void 0;
-const express_1 = require("express");
-exports.maquettesRouter = (0, express_1.Router)();
-exports.maquettesRouter.get('/', (req, res) => {
-    return res.json("OK");
-});
