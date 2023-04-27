46afb8cd8def9fc4958969c1aaaec0000cd03f4d
Pokoki <kennysu78@hotmail.com>
2023-04-27 15:59:54 +0200

    Starting Maquette schema

--- src/Maquettes/maquette.entity.ts
+++ src/Maquettes/maquette.entity.ts
@@ -1,5 +1,25 @@
-export interface IMaquette {
+import Joi from "joi";
+import mongoose from '../services/mongoose';
+
+export interface IMaquette extends mongoose.Document {
     name: string;
+    idArtiste: number;
     url: string;
     dateSubmit: Date;
 }
+
+export const MaquetteValidationSchema = Joi.object({
+    name: Joi.string().required(),
+    idArtiste: Joi.number().required().positive(),
+    url: Joi.string().required(),
+    dateSubmit: Joi.date().required()
+});
+
+export const MaquetteSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    idArtiste: { type: Number, required: true },
+    url: { type: String, required: true },
+    dateSubmit: { type: Date, required: true }
+});
+
+export default mongoose.model<IMaquette>('Maquette', MaquetteSchema);
