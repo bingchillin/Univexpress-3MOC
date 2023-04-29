87d6a20f435260368b3d344cb3c806671343eb6a
Meithal <hurin8888@gmail.com>
2023-04-29 15:28:45 +0200

    l'admin ne peut pas upload de maquettes (a tester)

--- src/Maquettes/Maquettes.Controller.ts
+++ src/Maquettes/Maquettes.Controller.ts
@@ -1,7 +1,15 @@
 import { Router, Request, Response } from "express";
+import { forbidAuthMiddleware } from "../middlewares/authMiddleware";
 
 export const maquettesRouter = Router();
 
 maquettesRouter.get("/", (req: Request, res: Response)=> {
+    return res.json("OK");
+});
+
+maquettesRouter.post(
+    "/", 
+    forbidAuthMiddleware(["admin", "manager", "user"]), 
+    (req: Request, res: Response) => {
     return res.json("OK");
 });
--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,29 +1,38 @@
 import { NextFunction, Response } from "express";
-import { Role } from "../Users/user.entity";
+import { Role } from "../Users/User.Entity";
 import {Request} from 'express-jwt'
 
 
-const generateAuthMiddleware =  (
-    allowedRoles: Role[]
-    ) => {
-    return (
-        req: Request,
-        res: Response,
-        next: NextFunction
-        ) => {
-            console.log(req.auth);
-            const currentRole: Role = req.auth?.role
+export const allowAuthMiddleware = (allowedRoles: Role[]) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        console.log(req.auth);
+        const currentRole: Role = req.auth?.role
 
-            if (!allowedRoles.includes(currentRole)) {
-                res.status(401).send({
-                    status: 401,
-                    message: "You cannot access this resource."
-                })
-            } else {
-                next();
-            }
-
+        if (!allowedRoles.includes(currentRole)) {
+            res.status(401).send({
+                status: 401,
+                message: "You cannot access this resource."
+            })
+        } else {
+            next();
+        }
     }
 }
 
-export default generateAuthMiddleware;
+export const forbidAuthMiddleware = (forbidRoles: Role[]) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        console.log(req.auth);
+
+        const currentRole: Role = req.auth?.role
+
+        if (forbidRoles.includes(currentRole)) {
+            res.status(401).send({
+                status: 401,
+                message: "You cannot access this resource."
+            })
+        } else {
+            next();
+        }
+
+    }
+}
